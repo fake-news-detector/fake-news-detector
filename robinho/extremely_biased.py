7bbce98a3b1e634b04edf7479c16ad0a8b2e1a56
Rogerio Chaves <rogeriocfj@gmail.com>
2017-10-20 02:53:08 -0200

    Add extremely biased classifier

--- robinho/extremely_biased.py
+++ robinho/extremely_biased.py
@@ -1,38 +1,49 @@
-import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfTransformer
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.pipeline import Pipeline
 import pickle
-from robinho.data import load_data
+from robinho.data import load_links
 
 
 def classifier():
     return Pipeline([
-        ('vect', CountVectorizer()),
+        ('vect', CountVectorizer(ngram_range=(1, 1))),
         ('tfidf', TfidfTransformer()),
         ('sampling', RandomUnderSampler()),
         ('clf', MultinomialNB()),
     ])
 
 
+def features_labels():
+    df = load_links()
+
+    df["is_biased"] = [category_id == 4 for category_id in df["category_id"]]
+
+    X = df["title"]
+    y = df["is_biased"]
+
+    return X, y
+
+
 def train():
-    X, y = load_data()
+    X, y = features_labels()
 
     print("Fitting data...")
     clf = classifier()
     clf = clf.fit(X, y)
 
     print("Saving model...")
-    pickle.dump(clf, open('model.pkl', 'wb'))
+    with open('data/extremely_biased.pkl', "wb") as f:
+        pickle.dump(clf, f)
 
 
-def load_model():
+def load():
     print("Loading model...")
-    return pickle.load(open('model.pkl', 'rb'))
+    with open('data/extremely_biased.pkl', "rb") as f:
+        return pickle.load(f)
 
 
-def predict(titles):
-    return load_model().predict(titles)
+def predict(title):
+    return load().predict_proba([title])[0][1]
