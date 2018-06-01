3fa42e2aceec5e28093de7e055e0e52aef5f26fa
Rogerio Chaves <rogeriocfj@gmail.com>
2018-06-01 18:49:09 -0300

    Revert "Add quoted_status and entities to the twitter search api"
    This reverts commit cae97afd9bfcef158ee7597779807152d6cc8f33.

--- api/src/endpoints/twitter.rs
+++ api/src/endpoints/twitter.rs
@@ -122,9 +122,6 @@ struct SearchTweet {
     user: Option<SearchUser>,
     #[serde(skip_serializing_if = "Option::is_none")]
     retweeted_status: Option<IdAndUser>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    quoted_status: Option<IdAndUser>,
-    entities: SearchEntities,
 }
 
 #[derive(Debug, Serialize)]
@@ -138,11 +135,6 @@ struct IdAndUser {
     id_str: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     user: Option<SearchUser>,
-}
-
-#[derive(Debug, Serialize)]
-struct SearchEntities {
-    user_mentions: Vec<SearchUser>,
 }
 
 fn parse_statuses(result: &egg_mode::Response<egg_mode::search::SearchResult>) -> Vec<SearchTweet> {
@@ -169,31 +161,6 @@ fn parse_statuses(result: &egg_mode::Response<egg_mode::search::SearchResult>) -
                         }),
                     }
                 }),
-                quoted_status: tweet.to_owned().quoted_status.map(|quote| {
-                    IdAndUser {
-                        id_str: format!("{}", quote.id),
-                        user: quote.to_owned().user.map(|user| {
-                            SearchUser {
-                                id_str: format!("{}", user.id),
-                                screen_name: user.screen_name,
-                            }
-                        }),
-                    }
-                }),
-                entities: SearchEntities {
-                    user_mentions: tweet
-                        .to_owned()
-                        .entities
-                        .user_mentions
-                        .iter()
-                        .map(|user| {
-                            SearchUser {
-                                id_str: format!("{}", user.id),
-                                screen_name: user.to_owned().screen_name,
-                            }
-                        })
-                        .collect(),
-                },
             }
         })
         .collect()
