a55cc69b396d70eaef927baff3392c0ff1c3a1b2
Rogerio Chaves <rogeriocfj@gmail.com>
2019-05-08 21:58:29 +0200

    Import on alphabetic order

--- robinho/__main__.py
+++ robinho/__main__.py
@@ -1,13 +1,13 @@
 import time
 print("Loading models...")
 start = time.time()
-import sys
-import robinho.server as server
-import robinho.bot.telegram_bot as telegram_bot
-import robinho.bot.terminal_bot as terminal_bot
-import robinho.bot.messages.chitchat as chitchat
-from robinho.model import Robinho
 from robinho.utils import current_ram
+from robinho.model import Robinho
+import robinho.bot.messages.chitchat as chitchat
+import robinho.bot.terminal_bot as terminal_bot
+import robinho.bot.telegram_bot as telegram_bot
+import robinho.server as server
+import sys
 
 end = time.time()
 print("Done! Models loaded in", "{:.1f}".format(end - start),
